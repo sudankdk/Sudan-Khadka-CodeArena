256ecc81e9773b4f29e1d71e9428d506d91c242f
sudankdk <sudankhadka345@gmail.com>
2025-11-01 11:48:33 +0545

    configs setup

---
+++ configs/appConfigs.go
@@ -0,0 +1,35 @@
+package configs
+
+import (
+	"errors"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type AppConfigs struct {
+	PORT string
+	DSN  string
+}
+
+func SetUpEnv() (AppConfigs, error) {
+	if os.Getenv("APP_ENV") == "dev" {
+		if err := godotenv.Load(); err != nil {
+			log.Fatal("Error loading .env file")
+		}
+	}
+	port := os.Getenv("PORT")
+	if port == "" {
+		return AppConfigs{}, errors.New("Port missing in env")
+	}
+	dsn := os.Getenv("DSN")
+	if dsn == "" {
+		return AppConfigs{}, errors.New("DSN missing in env")
+	}
+	cfg := AppConfigs{
+		PORT: port,
+		DSN:  dsn,
+	}
+	return cfg, nil
+}
