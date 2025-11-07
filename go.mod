4a4692420eb5ab75cc79f98066ec5436835ba0f0
sudankdk <sudankhadka345@gmail.com>
2025-11-07 06:35:05 +0545

    auth helpers done

--- go.mod
+++ go.mod
@@ -1,19 +1,22 @@
 module github.com/sudankdk/codearena
 
-go 1.22.2
+go 1.24.0
+
+toolchain go1.24.10
 
 require github.com/stretchr/testify v1.11.1
 
 require (
+	github.com/golang-jwt/jwt/v5 v5.3.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/pgx/v5 v5.6.0 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	golang.org/x/crypto v0.31.0 // indirect
-	golang.org/x/sync v0.10.0 // indirect
-	golang.org/x/text v0.21.0 // indirect
+	golang.org/x/crypto v0.43.0 // indirect
+	golang.org/x/sync v0.17.0 // indirect
+	golang.org/x/text v0.30.0 // indirect
 )
 
 require (
@@ -31,7 +34,7 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
-	golang.org/x/sys v0.28.0 // indirect
+	golang.org/x/sys v0.37.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	gorm.io/driver/postgres v1.6.0
 	gorm.io/gorm v1.31.1
---
+++ internal/helper/auth.go
@@ -0,0 +1,118 @@
+package helper
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+	"github.com/sudankdk/codearena/internal/domain"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type Auth struct {
+	Secret string
+}
+
+func SetupAuth(s string) *Auth {
+	return &Auth{
+		Secret: s,
+	}
+}
+
+func (a Auth) CreateHash(p string) (string, error) {
+	if len(p) < 6 {
+		return "", errors.New("Password must be more than 6 characters.")
+	}
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(p), 10)
+	if err != nil {
+		return "", errors.New("error in generating hash")
+	}
+	return string(hashedPassword), nil
+}
+
+func (a Auth) VerifyHash(p, hp string) bool {
+	if err := bcrypt.CompareHashAndPassword([]byte(hp), []byte(p)); err != nil {
+		return false
+	}
+	return true
+}
+
+func (a Auth) GenerateToken(id uuid.UUID, email, role string) (string, error) {
+	if id == uuid.Nil || email == "" || role == "" {
+		return "", errors.New("required fields cannot be empty")
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"id":          id,
+		"email":       email,
+		"role":        role,
+		"expiry_date": time.Now().Add(24 * 30 * time.Hour).Unix(),
+	})
+	signedToken, err := token.SignedString([]byte(a.Secret))
+	if err != nil {
+		return "", fmt.Errorf("error in signing token :%d", signedToken)
+	}
+	return signedToken, nil
+}
+
+func (a Auth) VerifyToken(token string) (domain.User, error) {
+	splitToken := strings.Split(token, " ")
+	if len(splitToken) != 2 {
+		return domain.User{}, errors.New("bearer missing or invalid token")
+	}
+	keyWord := splitToken[0]
+	if keyWord != "Bearer" {
+		return domain.User{}, errors.New("Bearer Missing")
+	}
+	parsedToken, err := jwt.Parse(splitToken[1], func(t *jwt.Token) (any, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return domain.User{}, fmt.Errorf("Invalid signing method")
+		}
+		return []byte(a.Secret), nil
+	})
+	if err != nil {
+		return domain.User{}, err
+	}
+	if claims, ok := parsedToken.Claims.(jwt.MapClaims); ok && parsedToken.Valid {
+		expiryDate, ok := claims["expiry_date"].(float64)
+		if !ok {
+			return domain.User{}, errors.New("invalid expiry date type")
+
+		}
+		if time.Now().Unix() > int64(expiryDate) {
+			return domain.User{}, errors.New("token is expired")
+		}
+		user := domain.User{}
+		user.Email = claims["email"].(string)
+		user.Role = claims["role"].(string)
+		user.ID = claims["id"].(uuid.UUID)
+		return user, nil
+	}
+	return domain.User{}, nil
+}
+
+func (a Auth) Authorize(ctx *fiber.Ctx) error {
+	authHeaders := ctx.GetReqHeaders()["Authorization"]
+	var authHeader string
+	if len(authHeaders) > 0 {
+		authHeader = authHeaders[0]
+	}
+	user, err := a.VerifyToken(authHeader)
+	if err != nil {
+		return ctx.Status(401).JSON(&fiber.Map{
+			"message": "Authorization Failed",
+			"reason":  err,
+		})
+	}
+	ctx.Locals("user", user)
+	return ctx.Next()
+}
+
+func (a Auth) CurrentUserInfo(ctx *fiber.Ctx) (domain.User, error) {
+	user := ctx.Locals("user")
+
+	return user.(domain.User), nil
+}
